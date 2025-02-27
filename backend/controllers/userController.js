263fc72dcf6bdd9e68c744a5a6d23a2433be046b
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-02-27 12:00:42 -0400

    connect to user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,26 @@
 import asyncHandler from 'express-async-handler';
 
+//auth user/set token
 export const authUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'success' });
+  res.status(200).json({ message: 'success' });
+});
+
+//register a user
+export const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'register user' });
+});
+
+//logout a user
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'logout user' });
+});
+
+//get user profile
+export const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'user profile' });
+});
+
+//update user profile
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'update user profile' });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import userRoutes from './routes/userRoutes.js';
 import express from 'express';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,6 +9,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => res.send('API running'));
 
