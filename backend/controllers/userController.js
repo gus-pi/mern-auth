d312e709251b844e44fa2d30b673c5df2d930aaa
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-02-27 15:40:32 -0400

    add register user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import { User } from '../models/userModel.js';
 
 //auth user/set token
 export const authUser = asyncHandler(async (req, res) => {
@@ -7,6 +8,31 @@ export const authUser = asyncHandler(async (req, res) => {
 
 //register a user
 export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user');
+  }
+
   res.status(200).json({ message: 'register user' });
 });
 
