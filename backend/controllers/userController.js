644a9f445f8d5775098e569bd2797c0478ab20d0
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-02-28 08:56:27 -0400

    add login user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,22 @@ import generateToken from '../utils/generateToken.js';
 
 //auth user/set token
 export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
+
   res.status(200).json({ message: 'success' });
 });
 
