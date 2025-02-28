0bcef0a89ad450e33a2f534384542d64d3f11b01
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-02-28 13:51:18 -0400

    add homepage and hero components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1>MERN AUTH</h1>;
+      <HomePage />
     </>
   );
 };
