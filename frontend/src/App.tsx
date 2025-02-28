f6b6b5d3f3e08219f5647ca6eb5e1ea7fbcdd2a8
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-02-28 13:44:11 -0400

    add header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import Header from './components/Header';
+
 const App = () => {
-  return <h1>MERN AUTH</h1>;
+  return (
+    <>
+      <Header />
+      <h1>MERN AUTH</h1>;
+    </>
+  );
 };
 
 export default App;
