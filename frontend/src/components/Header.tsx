b6c8c461ecdd9c74f36a9c086670b00adbeb91f8
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-05 15:34:06 -0400

    add auth header links

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,26 +1,43 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { RootState } from '../store';
 
 const Header = () => {
+  const { userInfo } = useSelector((state: RootState) => state.auth);
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MERN AUTH</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            MERN AUTH
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Link to="/login">
-                <Nav.Link>
-                  <FaSignInAlt /> Sign In
-                </Nav.Link>
-              </Link>
-              <Link to="/register">
-                <Nav.Link>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
-              </Link>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id="username">
+                    <NavDropdown.Item as={Link} to="/profile">
+                      Profile
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item>Log out</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/login">
+                    <FaSignInAlt /> Sign In
+                  </Nav.Link>
+
+                  <Nav.Link as={Link} to={'/register'}>
+                    <FaSignOutAlt /> Sign Up
+                  </Nav.Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
