98854e3d49c9964d256b337a6f7c19dc217abee7
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-05 16:08:58 -0400

    start profile and make it private

---
+++ frontend/src/components/PrivateRoute.tsx
@@ -0,0 +1,10 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { RootState } from '../store';
+
+const PrivateRoute = () => {
+  const { userInfo } = useSelector((state: RootState) => state.auth);
+  return userInfo ? <Outlet /> : <Navigate to="login" replace />;
+};
+
+export default PrivateRoute;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,8 @@ import LoginPage from './pages/LoginPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
 import { Provider } from 'react-redux';
 import store from './store.ts';
+import ProfilePage from './pages/ProfilePage.tsx';
+import PrivateRoute from './components/PrivateRoute.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +23,9 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
     </Route>
   )
 );
